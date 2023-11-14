34a2ab3510818ac601e3e2a1d2c030df9c5e625a
Tachibana Shin <tachibshin@duck.com>
2023-11-14 13:00:04 +0700

    test

--- scripts/upload-plugin.ts
+++ scripts/upload-plugin.ts
@@ -4,25 +4,38 @@ import { uploadPlugin } from "../runners/upload-plugin.ts"
 
 const issue_number = parseInt(Deno.args[1])
 
-if (Number.isNaN(issue_number)) {
-  throw new Error("ID issue invalid")
+let comment_body: string
+try {
+  if (Number.isNaN(issue_number)) {
+    throw new Error("ID issue invalid")
+  }
+
+  const issue = await octokit.issues.get({
+    owner: OK_OWNER,
+    repo: OK_REPO,
+    issue_number: issue_number
+  })
+
+  // Parse the issue body
+  const issueBody = issue.data.body
+
+  // Assuming the value of 'id: url' is on its own line like 'url: https://example.com/'
+  const urlLine = issueBody?.split("\n").find((line) => line.startsWith("url:"))
+
+  if (urlLine) {
+    const urlValue = urlLine.split(":")[1].trim()
+    console.log(await uploadPlugin(Deno.args[0], urlValue))
+  } else {
+    throw new Error("URL not found in issue body")
+  }
+  comment_body = "The script ran successfully!"
+} catch (err) {
+  comment_body = "There was an error running the script. Error details: " + err
 }
 
-const issue = await octokit.issues.get({
+await octokit.rest.issues.createComment({
   owner: OK_OWNER,
   repo: OK_REPO,
-  issue_number: issue_number
+  issue_number: issue_number,
+  body: comment_body
 })
-
-// Parse the issue body
-const issueBody = issue.data.body
-
-// Assuming the value of 'id: url' is on its own line like 'url: https://example.com/'
-const urlLine = issueBody?.split("\n").find((line) => line.startsWith("url:"))
-
-if (urlLine) {
-  const urlValue = urlLine.split(":")[1].trim()
-  console.log(await uploadPlugin(Deno.args[0], urlValue))
-} else {
-  throw new Error("URL not found in issue body")
-}
