dfc6653808dc1d10c7d8d04ab2f53edbea64cf0e
Tachibana Shin <tachibshin@duck.com>
2023-11-14 12:41:03 +0700

    update action

--- scripts/upload-plugin.ts
+++ scripts/upload-plugin.ts
@@ -1,5 +1,28 @@
-import { uploadPlugin } from "../runners/upload-plugin.ts";
+import { octokit } from "../boot/octokit.ts"
+import { OK_OWNER, OK_REPO } from "../constants.ts"
+import { uploadPlugin } from "../runners/upload-plugin.ts"
 
-console.log(Deno.args)
+const issue_number = parseInt(Deno.args[1])
 
-// console.log(await uploadPlugin(Deno.args[0], Deno.args[1]))
+if (Number.isNaN(issue_number)) {
+  throw new Error("ID issue invalid")
+}
+
+const issue = await octokit.issues.get({
+  owner: OK_OWNER,
+  repo: OK_REPO,
+  issue_number: issue_number
+})
+
+// Parse the issue body
+const issueBody = issue.data.body
+
+// Assuming the value of 'id: url' is on its own line like 'url: https://example.com/'
+const urlLine = issueBody?.split("\n").find((line) => line.startsWith("url:"))
+
+if (urlLine) {
+  const urlValue = urlLine.split(":")[1].trim()
+  console.log(await uploadPlugin(Deno.args[0], urlValue))
+} else {
+  throw new Error("URL not found in issue body")
+}
